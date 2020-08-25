9149f1b41a6a6e1ea6712ccdd4bcf7e9e568e927
Sindatus <70113859+Sindatus@users.noreply.github.com>
2020-08-25 18:45:36 +0300

    "Wrong condition"

--- Calc.py
+++ Calc.py
@@ -1,12 +1,15 @@
-
 c = input("What do you want? (+; -; *; /; %; **0.5; **2; **)\n")
+d = ['+', '-', '*',  '/', '%', '**0.5', '**2', '**']
+if c != d:
+    print("Wrong condition")
+    exit(0)
 a = int(input("Enter number\n"))
 if c == '**0.5':
     print (a ** .5)
-    SystemExit
+    exit(0)
 elif c == '**2':
     print (a ** 2)
-    SystemExit
+    exit(0)
 else:
     b = int(input("Enter second number\n"))
 if c == '+':
@@ -21,3 +24,5 @@ elif c == '%':
     print (a % b)
 elif c == '**':
     print (a ** b)
+elif c != c:
+    print("Wrong condition")
