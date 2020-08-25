4f3181af238a0a7d5112a551daa51bf0a280368d
Sindatus <70113859+Sindatus@users.noreply.github.com>
2020-08-25 14:22:42 +0300

    Create Calc.py

---
+++ Calc.py
@@ -0,0 +1,20 @@
+print("Enter first number")
+a = int(input())
+print("Enter second number")
+b = int(input())
+print("What do you want? (+; -; *; /; %)")
+c = input()
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print (a - b)
+elif c == '*':
+    print (a * b)
+elif c == '/':
+    print (a / b)
+elif c == '%':
+    print (a % b)
+else:
+    print("wrong condition")
+
+'new Branch'
