b810d9b3548ad89f12dd975164e3462bd53711f5
Sindatus <70113859+Sindatus@users.noreply.github.com>
2020-08-28 20:17:05 +0300

    Global Update

--- Calc.py
+++ Calc.py
@@ -1,31 +1,21 @@
-move = input("What do you want? (+; -; *; /; %; **0.5; **2; **)\n")
-_array = ['+', '-', '*', '/', '%', '**0.5', '**2', '**']
+def calc(symbol, first, second):
+    return {"+": first + second, "-": first - second, "*": first * second, "/": first / second, "%": first % second,
+            "**": first ** second}[symbol]
 
-if move in _array:
-    print("")
-else:
-    print("Wrong condition")
-    exit(0)
 
-first_number = int(input("Enter number\n"))
+cycle_control = True
+while cycle_control:
+    mechanics = calc(input("What do you want? (+; -; *; /; %; **0.5; **2; **)\n"), int(input("Enter number:\t")),
+                     int(input("Enter number:\t")))
+    if not mechanics:
+        print("Try again!")
+    else:
+        print("Great")
+        print(mechanics)
 
-if move == '**0.5':
-    print(first_number ** .5)
-    exit(0)
-elif move == '**2':
-    print(first_number ** 2)
-    exit(0)
-else:
-    second_number = int(input("Enter second number\n"))
-if move == '+':
-    print(first_number + second_number)
-elif move == '-':
-    print(first_number - second_number)
-elif move == '*':
-    print(first_number * second_number)
-elif move == '/':
-    print(first_number / second_number)
-elif move == '%':
-    print(first_number % second_number)
-elif move == '**':
-    print(first_number ** second_number)
+    final_ask = input("Want to continue?: Yes/No\t")
+    if final_ask == "Yes":
+        print("Okay Bro")
+    elif final_ask == "No":
+        print("Okay Bro")
+        cycle_control = False
